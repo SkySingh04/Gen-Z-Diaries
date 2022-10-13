8a552ad91ef03e90348636a0074e7df0fbb49d7f
Akash Singh <114267538+Akash-Singh04@users.noreply.github.com>
2022-10-13 13:52:10 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL")
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
